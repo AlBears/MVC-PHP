045efbcea6430c4854620ec5a370d2ed70f6b302
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-07 19:13:12 +0300

    Add controller, dispatching

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-
+require '../App/Controllers/Posts.php';
 require '../Core/Router.php';
 
 $router = new Router();
@@ -8,24 +8,24 @@ $router = new Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
     
-// Display the routing table
- echo '<pre>';
-// var_dump($router->getRoutes());
- echo htmlspecialchars(print_r($router->getRoutes(), true));
- echo '</pre>';
+// // Display the routing table
+//  echo '<pre>';
+// // var_dump($router->getRoutes());
+//  echo htmlspecialchars(print_r($router->getRoutes(), true));
+//  echo '</pre>';
 
-//Match the route with inserted
-$url = $_SERVER['QUERY_STRING'];
+// //Match the route with inserted
+// $url = $_SERVER['QUERY_STRING'];
 
-if($router->match($url)) {
-	echo '<pre>';
-	var_dump($router->getParams());
-	echo '</pre>';
-} else {
-	echo "No route found for URL '$url'";
-}
+// if($router->match($url)) {
+// 	echo '<pre>';
+// 	var_dump($router->getParams());
+// 	echo '</pre>';
+// } else {
+// echo "No route found for URL '$url'";
+// }
+
+$router->dispatch($_SERVER['QUERY_STRING']);
