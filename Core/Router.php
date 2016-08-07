abe6c7b6a0797b62db463077947b8a835e3c2878
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-07 17:45:53 +0300

    Get controller/action from fixed structure url

--- Core/Router.php
+++ Core/Router.php
@@ -22,12 +22,24 @@ class Router
 
     public function match($url)
     {
-        foreach ($this->routes as $route => $params) {
-            if ($url == $route) {
-                $this->params = $params;
-                return true;
+        //Using regular expressions to get controller and
+        //action from URL with fixed structure
+
+        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+
+        if (preg_match($reg_exp, $url, $matches)) {
+            $params = [];
+
+            foreach ($matches as $key => $match) {
+                if (is_string($key)) {
+                    $params[$key] = $match;
+                }
             }
+
+            $this->params = $params;
+            return true;
         }
+
         return false;
     }
 
