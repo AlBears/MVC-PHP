9df010097505f79493196eb01967424ef517a5c4
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-08 18:30:45 +0300

    Namespaces and autoload

--- public/index.php
+++ public/index.php
@@ -1,31 +1,19 @@
 <?php
+//Autoload Added
+spl_autoload_register(function ($class) {
+    $root = dirname(__DIR__);   // get the parent directory
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+    if (is_readable($file)) {
+        require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+});
 
-require '../App/Controllers/Posts.php';
-require '../Core/Router.php';
-
-$router = new Router();
-
+$router = new Core\Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
     
-// // Display the routing table
-//  echo '<pre>';
-// // var_dump($router->getRoutes());
-//  echo htmlspecialchars(print_r($router->getRoutes(), true));
-//  echo '</pre>';
-
-// //Match the route with inserted
-// $url = $_SERVER['QUERY_STRING'];
-
-// if($router->match($url)) {
-// 	echo '<pre>';
-// 	var_dump($router->getParams());
-// 	echo '</pre>';
-// } else {
-// echo "No route found for URL '$url'";
-// }
 
 $router->dispatch($_SERVER['QUERY_STRING']);
