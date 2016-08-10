40a2adf4f3a7a4ffe587c989b1d15151fd3f3400
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-10 18:41:59 +0300

    Add Twig template

--- Core/View.php
+++ Core/View.php
@@ -17,4 +17,16 @@ class View
             echo "$file not found";
         }
     }
+
+    public static function renderTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) {
+            $loader = new \Twig_Loader_Filesystem('../App/Views');
+            $twig = new \Twig_Environment($loader);
+        }
+
+        echo $twig->render($template, $args);
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+
+require_once '../vendor/Twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
 //Autoload Added
 spl_autoload_register(function ($class) {
     $root = dirname(__DIR__);   // get the parent directory
