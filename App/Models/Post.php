4893c1e3f6458d10c6453dc9c9910195291975e3
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-11 17:55:10 +0300

    Core Model class

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -5,21 +5,16 @@ namespace App\Models;
 use PDO;
 
 
-class Post
+class Post extends \Core\Model
 {
 
     public static function getAll()
     {
-        $host = 'localhost';
-        $dbname = 'mvc';
-        $username = 'root';
-        $password = 'root';
     
         try {
-            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
-                          $username, $password);
+            $db = static::getDB();
 
-            $stmt = $db->query('SELECT id, title, content FROM posts
+            $stmt = $db->query('SELECT id, title, content FROM posts 
                                 ORDER BY created_at');
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -30,3 +25,4 @@ class Post
         }
     }
 }
+
