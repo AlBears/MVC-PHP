f26343ea7bb12e7a60fa3e470103d1cd8593a4da
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-07-22 19:53:15 +0300

    find user by email

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Models\User;
 
 /**
  * Login controller
@@ -19,5 +20,11 @@ class Login extends \Core\Controller
     public function newAction()
     {
         View::renderTemplate('Login/new.html');
+    }
+
+    public function createAction()
+    {
+      $user = User::findByEmail($_POST['email']);
+      var_dump($user);
     }
 }
