caeaab4104c9a0e69bd498d06c3a66c0d533982b
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-07-25 23:17:29 +0300

    add session to login

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -31,7 +31,7 @@ class Login extends \Core\Controller
         $user = User::authenticate($_POST['email'], $_POST['password']);
 
         if ($user) {
-
+            $_SESSION['user_id'] = $user->id;
             $this->redirect('/');
 
         } else {
