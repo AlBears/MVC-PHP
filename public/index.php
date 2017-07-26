a8ca9b5335b04602b34c0c69b3a63140e8de1650
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-07-26 22:56:12 +0300

    session destroy

--- public/index.php
+++ public/index.php
@@ -15,6 +15,7 @@ session_start();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('login', ['controller' => 'Login', 'action' => 'new']);
+$router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 $router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
