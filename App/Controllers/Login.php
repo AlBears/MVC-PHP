0ca2764cb18da17b459af581d369648005e0a1a1
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-10-08 11:50:08 +0300

    redirect back to originally requested page

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -33,7 +33,7 @@ class Login extends \Core\Controller
 
         if ($user) {
             Auth::login($user);
-            $this->redirect('/');
+            $this->redirect(Auth::getReturnToPage());
 
         } else {
             $email = $_POST['email'];
