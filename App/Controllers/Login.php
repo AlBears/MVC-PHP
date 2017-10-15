8f9fb683e18dd8047c35aa7f50b53c12849cff1d
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-10-15 11:38:34 +0300

    remember_me

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -32,6 +32,8 @@ class Login extends \Core\Controller
     {
         $user = User::authenticate($_POST['email'], $_POST['password']);
 
+        $remember_me = isset($_POST['remember_me']);
+
         if ($user) {
             Auth::login($user);
 
@@ -44,6 +46,7 @@ class Login extends \Core\Controller
 
             View::renderTemplate('Login/new.html', [
               'email' => $_POST['email'],
+              'remember_me' => $remember_me
             ]);
         }
     }
