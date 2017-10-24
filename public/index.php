95855ffd0cb85c41fb8e2a169e63a1d2a26902d1
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-10-24 21:47:26 +0300

    add action yo get token from url

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ session_start();
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('login', ['controller' => 'Login', 'action' => 'new']);
 $router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
+$router->add('password/reset/{token:[\da-f]+}', ['controller' => 'Password', 'action' => 'reset']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 $router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
